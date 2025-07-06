7df002a7740a71b788174b05fe12988fd1200b79
phirawitDev <phirawit.chanta>
2025-07-06 14:12:36 +0700

    d

--- app/component/Headers.ts
+++ app/component/Headers.ts
@@ -1,6 +1,6 @@
 export function getAuthHeaders() {
-  const tokenname = process.env.NEXT_PUBLIC_TOKEN_NAME;
-  const token = localStorage.getItem(tokenname!);
+  const tokenname = String(process.env.NEXT_PUBLIC_TOKEN_NAME);
+  const token = localStorage.getItem(tokenname);
 
   return {
     Authorization: `Bearer ${token}`,
